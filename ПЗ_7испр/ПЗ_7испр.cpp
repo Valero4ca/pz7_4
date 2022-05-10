3e5a074c46a341f7a50603c72f06faba9e8cfa89
Valeria Makarevich <valeriamakarevich@outlook.com>
2022-05-10 19:52:01 +0300

    ещё раз изменила

---
+++ ПЗ_7испр/ПЗ_7испр.cpp
@@ -0,0 +1,86 @@
+﻿#include <iostream>
+
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    struct time {
+        int hours;
+        int minutes;
+    };
+
+    struct raspisaniye {
+        int  reis;
+        char bus_type[64];
+        char punkt_naznachenya[64];
+        time  vremya_otpravlenya;
+        time  vremya_pribitiya;
+    };
+
+    raspisaniye Raspisaniye_avtovokzala[3];
+
+    for (int i = 0; i < 3; i++) {
+        Raspisaniye_avtovokzala[i].reis = i + 1;
+
+        std::cout << "Рейс" << i + 1 << "." << "Введите тип автобуса: " << std::endl;
+        std::cin >> Raspisaniye_avtovokzala[i].bus_type;
+
+        std::cout << "Рейс" << i + 1 << "." << "Введите пункт назначения : " << std::endl;
+        std::cin >> Raspisaniye_avtovokzala[i].punkt_naznachenya;
+
+        std::cout << "Рейс" << i + 1 << "." << "Введите время отправления(часы): " << std::endl;
+        std::cin >> Raspisaniye_avtovokzala[i].vremya_otpravlenya.hours;
+        std::cout << "Рейс" << i + 1 << "." << "Введите время отправления(минуты): " << std::endl;
+        std::cin >> Raspisaniye_avtovokzala[i].vremya_otpravlenya.minutes;
+
+        std::cout << "Рейс" << i + 1 << "." << "Введите время прибытия(часы): " << std::endl;
+        std::cin >> Raspisaniye_avtovokzala[i].vremya_pribitiya.hours;
+        std::cout << "Рейс" << i + 1 << "." << "Введите время прибытия(минуты): " << std::endl;
+        std::cin >> Raspisaniye_avtovokzala[i].vremya_pribitiya.minutes;
+    }
+
+    for (int i = 0; i < 3; i++) {
+
+        std::cout << "Рейс" << i + 1 << "." << "Тип автобуса: "
+            << Raspisaniye_avtovokzala[i].bus_type << std::endl;
+        std::cout << "Рейс" << i + 1 << "." << "Пункт назначения: "
+            << Raspisaniye_avtovokzala[i].punkt_naznachenya << std::endl;
+        std::cout << "Рейс" << i + 1 << "." << "Время отправления: "
+            << Raspisaniye_avtovokzala[i].vremya_otpravlenya.hours << ":" << Raspisaniye_avtovokzala[i].vremya_otpravlenya.minutes << std::endl;
+        std::cout << "Рейс" << i + 1 << "." << "Время прибытия: "
+            << Raspisaniye_avtovokzala[i].vremya_pribitiya.hours << ":" << Raspisaniye_avtovokzala[i].vremya_pribitiya.minutes << std::endl;
+    }
+
+    std::cout << "Пункт назначения:" << std::endl;
+
+    for (int i = 0; i < 3; i++) {
+        std::cout << "[" << i << "]" << Raspisaniye_avtovokzala[i].punkt_naznachenya << std::endl;
+    }
+
+    int vybraniy_marshrut;
+    std::cin >> vybraniy_marshrut;
+
+    time vremia;
+
+    std::cout << "Время прибытия(часы): " << std::endl;
+    std::cin >> vremia.hours;
+    std::cout << "Время прибытия(минуты):" << std::endl;
+    std::cin >> vremia.minutes;
+
+    if (vremia.hours <= Raspisaniye_avtovokzala[vybraniy_marshrut].vremya_pribitiya.hours) {
+        if (vremia.minutes < Raspisaniye_avtovokzala[vybraniy_marshrut].vremya_pribitiya.minutes) {
+            std::cout << "Можно ехать !";
+        }
+        else {
+            std::cout << "ехать нелья!";
+        }
+    }
+    else {
+        std::cout << "Получите результат!";
+    }
+
+   
+    return 0;
+
+}
---
+++ ПЗ_8/ПЗ_8.cpp
@@ -0,0 +1,126 @@
+﻿#include <iostream>
+#include<stdio.h>
+#include<stdlib.h>
+#include<locale.h>
+
+using namespace std;
+ 
+void selectSort1(int A[],int n)
+{ 
+    int k, i, j, min;
+    
+    for (i = 0; i < n; i++) {
+        k = i;
+        min = A[i];
+        for(j=i+1;j<n;j++)
+            if (A[j] < min)
+            {
+                k = j;
+                min = A[j];
+            }
+        A[k] = A[i];
+        A[i] = min;
+    }
+    
+}
+
+void selectSort2(int A[], int size)
+{
+    int k, i, j;
+    for (i = 0; i < size - 1; i++)
+    {
+        for (j = i + 1, k = i; j < size; j++)
+            if (A[j] < A[k]) 
+                k = j;
+        int c = A[k];
+        A[k] = A[i];
+        A[i] = c;
+    }
+}
+
+int main()
+{
+    setlocale(LC_ALL, "RUS");
+    int size;
+    int A[1000];
+    cout << "Количество элементов >";
+    cin >> size;
+    for (int i = 0; i < size; i++)
+    {
+        cout << i + 1 << " элемент >";
+        cin >> A[i];
+    }
+    selectSort1(A, size);
+    cout << "результат сортировки. Метод #1\n";
+    for (int i = 0; i < size; i++)
+        cout << A[i] << "";
+    cout << "\n Массив 2";
+    for (int i = 0; i < size; i++)
+    {
+        A[i] = rand() % 11 - 5;
+        cout << A[i] << ".>";
+    }
+    selectSort2(A, size);
+    cout << "результат сортировки.Метод #2\n";
+    for (int i = 0; i < size; i++)
+        cout << A[i] << "";
+
+
+
+    }
+
+
+
+
+void insertSort(int *A, int size)
+{
+    int t, i, j;
+    for (i = 1; i < size; i++)
+    {
+        t = A[i];
+        for (j = i - 1; j >= 0 && A[j] > t; j--)
+            A[j + 1] = A[j];
+        A[j + 1] = t;
+    }
+}
+void shellSort(int A[], int size)
+{
+    int i, j, c, d = size;
+    d = d / 2;
+    while (d > 0)
+    {
+        for (i = 0; i < size - d; i++)
+        {
+            j = i;
+            while (j >= 0 && A[j] > A[j + d])
+            {
+                c = A[j];
+                A[j] = A[j + d];
+                A[j + d] = c;
+                j--;
+            }
+        }
+        d = d / 2;
+    }
+}
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+    srand(time(NULL));
+    clock_t t1, t2;
+    int A[1000];
+    int size;
+
+
+    сout << ">";
+    cin >> size;
+    for (int i = 0; i < size; i++) {
+        A[i] = rand() % (100 - 50) / 20;
+        cout << A[i] << "";
+
+        t1 = clock();
+        insertSort(int* A, int size);
+        t2 = clock();
+        cout << "Прошло " << t2 - t1 << " тактов времени\n";
+
+    }
